c4a9d8a16306d0daa040363507323fd58ef8c1ca
Mozta <viper_rafa_@hotmail.com>
2017-10-24 15:40:05 -0500

    Initial commit

---
+++ app/src/main/java/com/idit/testgooglemapinfragment/ConfiguracionFragment.java
@@ -0,0 +1,59 @@
+package com.idit.testgooglemapinfragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConfiguracionFragment extends Fragment implements OnMapReadyCallback {
+
+
+    GoogleMap map;
+
+    public ConfiguracionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_configuracion, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng pp = new LatLng(19.030148, -98.242044);
+        MarkerOptions option = new MarkerOptions();
+        option.position(pp).title("IDIT");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLng(pp));
+    }
+}
